4b5c8affd65c3ea9c0df12344d80b521e85aa762
Steve Williams <steve@symless.com>
2018-03-09 09:47:41 +0000

    Added initial subprocess handling

--- nginx-monitor.py
+++ nginx-monitor.py
@@ -5,6 +5,8 @@ from proc.core import find_processes
 import smtplib
 from email.mime.text import MIMEText
 from email.parser import Parser
+import subprocess
+from datetime import datetime, timedelta
 
 if __name__ == '__main__':
     page_size = os.sysconf('SC_PAGE_SIZE')
@@ -30,11 +32,43 @@ if __name__ == '__main__':
 
     print ('Cumulative nginx memory use = ', cumulative_percent)
 
-    if cumulative_percent > 80:
+    if cumulative_percent > 0:
         # Take action at the 80% mark - we want to schedule an nginx restart
         action_taken = 'Scheduling at for restart'
-    else:
 
+        # Check if we aleready have a restart pending
+        at_query = subprocess.run(['atq','-q','s'],stdout=subprocess.PIPE)
+        if at_query.returncode == 0:
+            print(at_query.stdout.decode("utf-8"))
+
+            # For now, if the length of the output was anything other than zero, we'll assumne somehting is
+            # scheduled
+            if len(at_query.stdout.decode("utf-8")) == 0:
+
+                print("Job not scheduled")
+                action_taken += "\n Job not scheduled - scheduling"
+
+
+                tomorrow = datetime.now() + timedelta(days=1)
+
+                print (tomorrow.date())
+
+                timespec = "{}{}{}0930".format(tomorrow.date().year,
+                                               tomorrow.date().month,
+                                               tomorrow.date().day)
+
+                sched_proc = subprocess.run(['echo','"systemctl restart nginx"','|','at','-q','s','-mv','-t',timespec],
+                                             stdout=subprocess.PIPE)
+            else:
+                print("Job already scheduled")
+                action_taken += "\n Job already scheduled in the at s queue - abort"
+
+        else:
+            action_taken += "\nReturn code from atq ({}) was {}".format(at_query.cmd,
+                                                                        at_query.returncode)
+
+
+    else:
         action_taken = 'No action taken'
 
     # Send ourselves an email - you will need an email template in cwd
